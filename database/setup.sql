45eca8fe5f07fa1e77a2718e058e2414ab177015
Daniyar Irishev <daniyar.irishev@kcl.ac.uk>
2018-11-22 10:02:11 +0000

    Add simple database

---
+++ database/setup.sql
@@ -0,0 +1,34 @@
+create database lab_project;
+use lab_project;
+
+create table Member (
+  idMember varchar(16) not null,
+  name varchar(255) not null,
+  surname varchar(255) not null,
+  rentingGames int check (rentingGames between 0 and 2),
+  extensions int,
+  violations int check (violations between 0 and 3),
+  firstViolation date,
+  latestViolation date,
+  banned boolean not null default true,
+  owingGame varchar(16),
+  primary key (idMember),
+  foreign key (owingGame) references Game (idGame)
+);
+
+create table Game (
+  idGame varchar(16) not null,
+  name varchar(255),
+  isAvailable boolean not null default true,
+  primary key (idGame)
+);
+
+create table Rentals (
+  idMember varchar(16) not null,
+  idGame varchar(16) not null,
+  startDate date,
+  endDate date,
+  primary key (idMember, idGame),
+  foreign key (idMember) references Member (idMember),
+  foreign key (idGame) references Game (idGame)
+);
