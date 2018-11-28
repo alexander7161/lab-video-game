2812a54fbdbb269c41e65682f064bc1d4ad91447
Alexander Davis <alexanderdavis.net@googlemail.com>
2018-11-28 22:07:51 +0000

    Create and delete games

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/', 'GameController@index')->name('index');
 Route::get('game', 'GameController@index');
 Route::get('game/{id}', 'GameController@getGame');
 Route::get('game/{id}/edit', 'GameController@editGame')->middleware('auth');
+Route::get('game/{id}/delete', 'GameController@deleteGame')->middleware('auth')->middleware('volunteer');
 Route::get('newGame', 'GameController@newGame')->middleware('auth');
 Route::post('newGame', 'GameController@createGame')->name('newGame');
 Route::get('members', 'UserController@getUsers')->middleware('auth');
