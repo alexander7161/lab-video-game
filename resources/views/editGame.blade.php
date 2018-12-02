b0902c1e49e1f5f53d91406eb66a9dd50c999b60
Alexander Davis <alexanderdavis.net@googlemail.com>
2018-12-02 22:18:46 +0000

    edit a game

--- resources/views/editGame.blade.php
+++ resources/views/editGame.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.app') 
 @section('content')
-<a href={{ "/game/{$game->id}/delete"}} onclick="return confirm('Are you sure you want to delete this item?');">
-    <button  type="button" class="btn">Delete</button>
-</a> {{ Form::open(array('url' => 'foo/bar')) }} {{ Form::close() }} editGame {{$game->id}}
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Edit ') }}{{$game->name}} <a style="float:right;" href={{ "/game/{$game->id}/delete"}} onclick="return confirm('Are you sure you want to delete this item?');">
+                    <button  type="button" class="btn">Delete</button>
+                </a> </div>
+                <div class="card-body">
+    @include('gameForm', ['game' => $game, 'new' => false])
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
