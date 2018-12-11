368684c1fd34d22054b5563abe4fb81fe64d6ab0
Alexander Davis <alexanderdavis.net@googlemail.com>
2018-12-11 23:45:54 +0000

    Account page

--- resources/views/game/leftpanel.blade.php
+++ resources/views/game/leftpanel.blade.php
@@ -3,7 +3,6 @@
     <div class="card-body">
         <h5 class="card-title" style="display: inline-block;">{{ $game->name }}
     @include('game.stars', ['rating' => $game->rating ]) <a href="{{ $game->recommendedurl}}" class="btn btn-warning btn-sm">View Review</a>
-
         </h5>
         @volunteer
         <a style="float: right;" href="{{ " /game/{$game->id}/edit"}}" class="btn btn-dark">Edit 
@@ -28,30 +27,3 @@
         </div>
     </div>
 </div>
-
-{{-- @if($game->rating)
-<script>
-    document.getElementById("stars").innerHTML = getStars({{ $game->rating}});
-
-    function getStars(rating) {
-
-        // Round to nearest half
-        rating = Math.round(rating * 2) / 2;
-        let output = [];
-
-        // Append all the filled whole stars
-        for (var i = rating; i >= 1; i--)
-        output.push('<i class="fa fa-star" aria-hidden="true" style="color: gold;"></i>&nbsp;');
-
-        // If there is a half a star, append it
-         if (i == .10) output.push('<i class="fa fa-star-half-o" aria-hidden="true" style="color: gold;"></i>&nbsp;');
-
-        // Fill the empty stars
-        for (let i = (10 - rating); i >= 1; i--)
-        output.push('<i class="fa fa-star-o" aria-hidden="true" style="color: gold;"></i>&nbsp;');
-
-        return output.join('');
-    }
-
-</script>
-@endif --}}
