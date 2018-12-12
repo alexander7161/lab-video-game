62bd0930016707ee8f5539caf5020cccc62a09b0
Alexander Davis <alexanderdavis.net@googlemail.com>
2018-12-12 17:59:19 +0000

    implement rules

--- resources/views/accountPage/leftPanel.blade.php
+++ resources/views/accountPage/leftPanel.blade.php
@@ -3,25 +3,48 @@ $u = $user;
 ?>
     <div class="card {{$u->banned?" bg-danger text-white ": " "}} " style="margin-bottom:8px;">
         <div class="card-body">
-            <h5 class="card-title" style="display: inline-block;">{{ $u->name }}</h5>
-            @if(!$u->volunteer)
+            <h5 class="card-title" style="display: inline-block;">{{ $u->name }}</h5>@if($u->volunteer)
+            <span class="badge badge-success">Volunteer</span>@endif @if($u->secretary)
+            <span class="badge badge-warning">Secretary</span>@endif @if(!$u->volunteer)
             <form style="float:right;" action="/account/{{$u->id}}/{{$u->banned?'unban':'ban'}}" method="GET">
                 <button type="submit" class="btn {{$u->banned? " btn-outline-light ":"btn-outline-dark "}}">{{$u->banned?"Unban" : "Ban" }}</button></form>
             @endif
             <div class="row">
                 <div class="col">
-                    <dl>
-                        <dt>Account created:</dt>
-                        <dd>
-                            <?php 
+                    <div class="row">
+                        <div class="col-sm">
+                            <dl>
+                                <dt>Account created:</dt>
+                                <dd>
+                                    <?php 
                             $timestamp = strtotime($u->created_at);
-                                echo date("Y/m/d - H:i", $timestamp); ?>
-                        </dd>
-                        @volunteer<dt>Banned:</dt>
-                        <dd>
-                            {{$u->banned?"True":"False"}}
-                        </dd>@endvolunteer
-                    </dl>
+                                echo date("d/m/Y - H:i", $timestamp); ?>
+                                </dd>
+                                @volunteer<dt>Banned:</dt>
+                                <dd>
+                                    {{$u->banned?"True":"False"}}
+                                </dd>@endvolunteer
+                            </dl>
+                        </div>
+                        @if($u->banned)
+                        <div class="col-sm">
+                            <dl>
+                                <dt>Ban started:</dt>
+                                <dd>
+                                    <?php 
+                                $timestamp = strtotime($ban[0]->datebanned);
+                                    echo date("d/m/Y - H:i", $timestamp); ?>
+                                </dd>
+                                <dt>Ban ends:</dt>
+                                <dd>
+                                    <?php 
+                                $timestamp = strtotime($ban[0]->banenddate);
+                                    echo date("d/m/Y - H:i", $timestamp); ?>
+                                </dd>
+                            </dl>
+                        </div>
+                        @endif
+                    </div>
                 </div>
             </div>
         </div>
